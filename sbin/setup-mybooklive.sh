fe3de70be017ec6fc537d3d5584a4e05ac09a1e6
dhameoelin <dhameoelin@gmail.com>
2012-03-27 13:18:10 +0000

    ipkg

--- sbin/setup-mybooklive.sh
+++ sbin/setup-mybooklive.sh
@@ -1,7 +1,8 @@
-#feed=http://ipkg.nslu2-linux.org/feeds/optware/ds101g/cross/stable/
-#ipk_name=$(wget -qO- $feed/Packages | awk '/^Filename: ipkg-opt/ {print $2}')
-#wget $feed/$ipk_name
-ipk_name='ipkg-opt_0.99.163-10_powerpc.ipk'
+feed=http://ipkg.nslu2-linux.org/feeds/optware/ds101g/cross/stable/
+ipk_name=$(wget -qO- $feed/Packages | awk '/^Filename: ipkg-opt/ {print $2}')
+wget $feed/$ipk_name
+##FIXME
+#ipk_name='ipkg-opt_0.99.163-10_powerpc.ipk'
 tar -xOvzf $ipk_name ./data.tar.gz | tar -C / -xzvf -
 echo "src/gz optware ${feed}" >> /opt/etc/ipkg.conf
 mv /opt/bin/ipkg /opt/bin/ipkg.bin
